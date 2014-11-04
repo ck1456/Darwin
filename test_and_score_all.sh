341f0650ab44a5733ae662502d6ea9ec68c73aba
Chris Keitel <ck1456@nyu.edu>
2014-11-03 19:24:33 -0500

    Added grader and new input data sets along with testing script.

---
+++ test_and_score_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+datadir="data/"
+solndir="soln/"
+invoke="java -jar GenMat.jar"
+#Problem 1
+for n in 1 2 3
+do
+  for type in A B
+  do
+    infile="${datadir}input_${n}.txt"
+    outfile="${solndir}output_${n}.txt"
+    echo "${type}: ${infile}"
+    ${invoke} ${type} ${infile} ${outfile}
+
+    java -jar ./spec/grader.jar ${type} "${infile}" "${outfile}"
+  done
+done
+echo
+
